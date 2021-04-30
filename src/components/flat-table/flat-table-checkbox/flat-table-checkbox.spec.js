1f8f5858f03efe1f0fe5197c6b1e4cb78f8f2cb2
Ed Leeks <edward.leeks@sage.com>
2021-04-30 11:43:07 +0100

    test(flat-table-checkbox): fix incorrect dom nesting

--- src/components/flat-table/flat-table-checkbox/flat-table-checkbox.spec.js
+++ src/components/flat-table/flat-table-checkbox/flat-table-checkbox.spec.js
@@ -13,15 +13,29 @@ describe("FlatTableCheckbox", () => {
   describe('the "as" prop is not passed in', () => {
     it('renders to match the expected styling for a "td" element', () => {
       expect(
-        TestRenderer.create(<FlatTableCheckbox />).toJSON()
+        TestRenderer.create(
+          <table>
+            <tbody>
+              <tr>
+                <FlatTableCheckbox />
+              </tr>
+            </tbody>
+          </table>
+        ).toJSON()
       ).toMatchSnapshot();
     });
 
     it('has the correct "data-element" when rendered as a "td"', () => {
       expect(
-        TestRenderer.create(<FlatTableCheckbox />).root.findByType(
-          StyledFlatTableCheckbox
-        ).props["data-element"]
+        TestRenderer.create(
+          <table>
+            <tbody>
+              <tr>
+                <FlatTableCheckbox />
+              </tr>
+            </tbody>
+          </table>
+        ).root.findByType(StyledFlatTableCheckbox).props["data-element"]
       ).toEqual("flat-table-checkbox-cell");
     });
   });
@@ -29,15 +43,29 @@ describe("FlatTableCheckbox", () => {
   describe('"th" is passed in via the "as" prop', () => {
     it('renders to match the expected styling for a "th" element when it is passed via the "as" prop', () => {
       expect(
-        TestRenderer.create(<FlatTableCheckbox as="th" />).toJSON()
+        TestRenderer.create(
+          <table>
+            <thead>
+              <tr>
+                <FlatTableCheckbox as="th" />
+              </tr>
+            </thead>
+          </table>
+        ).toJSON()
       ).toMatchSnapshot();
     });
 
     it('renders to match the expected styling for a "th" element', () => {
       expect(
-        TestRenderer.create(<FlatTableCheckbox as="th" />).root.findByType(
-          StyledFlatTableCheckbox
-        ).props["data-element"]
+        TestRenderer.create(
+          <table>
+            <thead>
+              <tr>
+                <FlatTableCheckbox as="th" />
+              </tr>
+            </thead>
+          </table>
+        ).root.findByType(StyledFlatTableCheckbox).props["data-element"]
       ).toEqual("flat-table-checkbox-header");
     });
   });
@@ -45,7 +73,15 @@ describe("FlatTableCheckbox", () => {
   describe('"click" event', () => {
     it('calls "stopPropagation" when the "Checkbox" is clicked', () => {
       const mockClick = jest.fn();
-      const wrapper = TestRenderer.create(<FlatTableCheckbox />);
+      const wrapper = TestRenderer.create(
+        <table>
+          <tbody>
+            <tr>
+              <FlatTableCheckbox />
+            </tr>
+          </tbody>
+        </table>
+      );
       wrapper.root
         .findByType(Checkbox)
         .props.onClick({ stopPropagation: mockClick });
@@ -55,7 +91,15 @@ describe("FlatTableCheckbox", () => {
 
   describe("when selectable prop is false", () => {
     it("should not render the checkbox", () => {
-      const wrapper = mount(<FlatTableCheckbox selectable={false} />);
+      const wrapper = mount(
+        <table>
+          <tbody>
+            <tr>
+              <FlatTableCheckbox selectable={false} />
+            </tr>
+          </tbody>
+        </table>
+      );
 
       expect(wrapper.find(Checkbox).exists()).toEqual(false);
     });
